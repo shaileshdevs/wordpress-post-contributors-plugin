19f0b01eb9c7e0410a2802444d366112eee39f3c
shaileshdevs <shaileshdevs@gmail.com>
2023-12-12 00:52:43 +0530

    Include files

--- shvsh-post-contributors/includes/class-init.php
+++ shvsh-post-contributors/includes/class-init.php
@@ -16,6 +16,28 @@ if ( ! class_exists( 'Shvsh_Post_Contributors\Init' ) ) {
 		 * Constructor.
 		 */
 		public function __construct() {
+			$this->include_files();
+		}
+
+		/**
+		 * Include the necessary files.
+		 *
+		 * @return void
+		 */
+		public function include_files() {
+			// Include the post contributor class for frontend and backend.
+			require_once plugin_dir_path( SHVSH_POST_CONTRIBUTORS_PLUGIN_FILE ) . 'includes/controller/class-post-contributors.php';
+			\Shvsh_Post_Contributors\Controller\Post_Contributors::get_instance();
+
+			if ( is_admin() ) {
+				// If admin backend then include the files.
+				require_once plugin_dir_path( SHVSH_POST_CONTRIBUTORS_PLUGIN_FILE ) . 'includes/admin/class-post-contributors-metabox.php';
+				\Shvsh_Post_Contributors\Admin\Post_Contributors_Metabox::get_instance();
+			} else {
+				// If frontend then include the files.
+				require_once plugin_dir_path( SHVSH_POST_CONTRIBUTORS_PLUGIN_FILE ) . 'includes/frontend/class-post-contributors-display.php';
+				\Shvsh_Post_Contributors\Frontend\Post_Contributors_Display::get_instance();
+			}
 		}
 
 		/**
